e9e8394c81defd6c0c3503eb7e5cf6e766910ae1
cavalleria <605370459@qq.com>
2020-06-28 05:57:22 -0700

    update README.md

--- backbone/proxylessnas.py
+++ backbone/proxylessnas.py
@@ -11,76 +11,8 @@
 import os
 import torch.nn as nn
 import torch.nn.init as init
-from .common import GDC
+from .common import GDC, get_activation_layer, ConvBlock, conv1x1_block, conv3x3_block
 
-def get_activation_layer(activation, out_channels):
-    """
-    Create activation layer from string/function.
-    """
-    assert (activation is not None)
-
-    if activation == "relu":
-        return nn.ReLU(inplace=True)
-    elif activation == "relu6":
-        return nn.ReLU6(inplace=True)
-    elif activation == "sigmoid":
-        return nn.Sigmoid()
-    elif activation == "prelu":
-        return nn.PReLU(out_channels)
-    else:
-        raise NotImplementedError()
-
-
-class ConvBlock(nn.Module):
-    """
-    Standard convolution block with Batch normalization and activation.
-    """
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding,
-                dilation=1, groups=1, bias=False, use_bn=True, bn_eps=1e-5,
-                activation=(lambda: nn.ReLU(inplace=True))):
-        super(ConvBlock, self).__init__()
-        self.activate = (activation is not None)
-        self.use_bn = use_bn
-        self.use_pad = (isinstance(padding, (list, tuple)) and (len(padding) == 4))
-
-        if self.use_pad:
-            self.pad = nn.ZeroPad2d(padding=padding)
-            padding = 0
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-            stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-        if self.use_bn:
-            self.bn = nn.BatchNorm2d(num_features=out_channels, eps=bn_eps)
-        if self.activate:
-            self.activ = get_activation_layer(activation, out_channels)
-
-    def forward(self, x):
-        if self.use_pad:
-            x = self.pad(x)
-        x = self.conv(x)
-        if self.use_bn:
-            x = self.bn(x)
-        if self.activate:
-            x = self.activ(x)
-        return x
-
-def conv1x1_block(in_channels, out_channels, stride=1, padding=0, groups=1,
-                bias=False, use_bn=True, bn_eps=1e-5,
-                activation=(lambda: nn.ReLU(inplace=True))):
-    """
-    1x1 version of the standard convolution block.
-    """
-    return ConvBlock(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride,
-        padding=padding, groups=groups, bias=bias, use_bn=use_bn, bn_eps=bn_eps, activation=activation)
-
-
-def conv3x3_block(in_channels, out_channels, stride=1, padding=1, dilation=1, groups=1,
-                bias=False, use_bn=True, bn_eps=1e-5, activation=(lambda: nn.ReLU(inplace=True))):
-    """
-    3x3 version of the standard convolution block.
-    """
-    return ConvBlock(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride,
-        padding=padding, dilation=dilation, groups=groups, bias=bias, use_bn=use_bn, bn_eps=bn_eps,
-        activation=activation)
 
 class ProxylessBlock(nn.Module):
     """
