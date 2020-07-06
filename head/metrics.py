25ef12c9505f4b7863cf00de487ac4a53ed72b14
Sacha <xsacha@gmail.com>
2020-07-06 12:53:46 +1000

    Update metrics.py
    Another usage of classnum

--- head/metrics.py
+++ head/metrics.py
@@ -490,7 +490,7 @@ class AirFace(nn.Module):
     def __init__(self, in_features, out_features, s=64., m=0.45):
         super(AirFace, self).__init__()
         self.classnum = out_features
-        self.kernel = Parameter(torch.Tensor(in_features, classnum))
+        self.kernel = Parameter(torch.Tensor(in_features, out_features))
         # initial kernel
         self.kernel.data.uniform_(-1, 1).renorm_(2, 1, 1e-5).mul_(1e5)
         self.m = m
